dab477b2b1b78bc4c3f37e7c96905a2fe8171f72
Yoonho Hann <83288181+hnnynh@users.noreply.github.com>
2024-03-15 11:59:30 +0900

    240315 레벨테스트 풀이

---
+++ Programmers/Lv.1/완주하지-못한-선수.cpp
@@ -0,0 +1,24 @@
+#include <string>
+#include <vector>
+#include <map>
+
+using namespace std;
+
+string solution(vector<string> participant, vector<string> completion) {
+    map<string, int> m;
+    
+    for(string &p : participant){
+        m[p]++;
+    }
+    
+    for(string &c : completion){
+        m[c]--;
+    }
+    
+    for(string &p : participant){
+        if(m[p] != 0)
+            return p;
+    }
+    
+    return "";
+}
